dde056f7cb8a82a05db4b2cc6e67f5cba712c839
Hadm Trust <ibnmvungi@live.com>
2017-09-20 11:13:53 +0300

    Adding new class

---
+++ src/gittest/MyClass.java
@@ -0,0 +1,56 @@
+/*
+  * 
+  *  Isamas
+  * 
+  *  An Inventroy and Sales Managment  for PHP
+  * 
+  *  This content is copyrighted. You are not allowed to copy,distribute,modify,use the content without acquiring permission from the owner.
+  * 
+  *  @package    Isamas
+  *  @author     Hadm Trust
+  *  @Developer  Fadhili Mvungi
+  *  @copyright  Copyright (c) 2017, Hadm Trust, Inc. (http://hadmtrust.com/)
+  *  @license    @hadmtrust.com
+  *  @link       http://hadmtrust.com
+  *  @email      developer@hadmtrust.com
+  *  @since      Version 1.0.0
+  * 
+ */
+package gittest;
+
+/**
+ *
+ * @author Ibn
+ */
+public class MyClass {
+    private int age;
+    private String name;
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
